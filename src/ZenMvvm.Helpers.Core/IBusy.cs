dec5c6a4469ddef834e14ed2753b0b75bc99d820
Zeebs <zeebsmail@gmail.com>
2020-12-16 16:56:49 +0200

    Initial commit

---
+++ src/ZenMvvm.Helpers.Core/IBusy.cs
@@ -0,0 +1,11 @@
+﻿namespace ZenMvvm.Helpers
+{
+    public interface IBusy
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is busy.
+        /// </summary>
+        /// <value><c>true</c> if this instance is busy; otherwise, <c>false</c>.</value>
+        bool IsBusy { get; set; }
+    }
+}
